7c0306662450bb11d5f69e07539306cbd7a467e9
Vladimir <v>
2025-12-03 11:07:43 +0100

    fixed

--- src/app/components/Counter/index.tsx
+++ src/app/components/Counter/index.tsx
@@ -2,14 +2,16 @@
 
 import { useAppSelector } from "@/store/store";
 
+import styles from './index.module.css'
+
 const Counter = () => {
 
     const count = useAppSelector(state => state.cart.items.length)
 
 
-    return <div>
-        Counter:   {count}
-    </div>
+    return (<div className={styles.counter}>
+        Counter: {count}
+    </div>)
 }
 
 export default Counter;
